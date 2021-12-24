41ca5967edf588f791a0b2a9d25a105796bade89
Daniel Vieira <daniel.vieira@cobasi.com.br>
2021-12-24 18:36:58 -0300

    adiciona o modulo de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { typeOrmConfigAsync } from './config/typeorm.config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    UserModule,
   ],
 })
 export class AppModule {}
